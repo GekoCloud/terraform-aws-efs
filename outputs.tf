064cd4e8b030188d6ca75f0cc18fa49f6a181905
David Pujadas <dpujadas@gmail.com>
2020-08-24 14:13:31 +0200

    Add arn output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "id" {
-  description = "EFS ID"
+  description = "File system ID"
   value       = aws_efs_file_system.this.id
+}
+
+output "arn" {
+  description = "File system ARN"
+  value       = aws_efs_file_system.this.arn
 }
